c02655f27b24ae13c5007fadf0e5c17e5016fc11
GannaRF <gannahinggil@refactory.id>
2022-09-24 14:23:40 +0700

    fix: query params

--- src/app/ui/views/Result/index.tsx
+++ src/app/ui/views/Result/index.tsx
@@ -1,69 +1,75 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 import {
   isOnlyWhiteSpace,
   queryString,
   serializeQuery,
-} from "@/app/infrastructures/misc/utils/useFormat"
-import { useHistory } from "react-router-dom"
-import { useAppDispatch, useAppSelector } from "@/app/ui/stores"
-import InputSearch from "@/app/ui/components/molecules/InputSearch"
-import Tabs from "@/app/ui/components/atoms/Tabs"
-import Icons from "@/app/ui/assets/Icons/index"
+} from "@/app/infrastructures/misc/utils/useFormat";
+import { useHistory } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "@/app/ui/stores";
+import InputSearch from "@/app/ui/components/molecules/InputSearch";
+import Tabs from "@/app/ui/components/atoms/Tabs";
+import Icons from "@/app/ui/assets/Icons/index";
 import {
   searchContent,
   searchImage,
   searchNews,
-  setQueryParams
-} from "@/app/ui/stores/actions/GoogleSearchAction"
-import { EnumTabsResult, ITEM_TABS_RESULT } from "@/app/infrastructures/misc/constants/common"
-import SearchAll from "./Search"
-import ImagesResult from "./Images"
-import NewsResult from "./News"
+  setQueryParams,
+} from "@/app/ui/stores/actions/GoogleSearchAction";
+import {
+  EnumTabsResult,
+  ITEM_TABS_RESULT,
+} from "@/app/infrastructures/misc/constants/common";
+import SearchAll from "./Search";
+import ImagesResult from "./Images";
+import NewsResult from "./News";
 
 const ResultPage = () => {
-  const params = queryString(window.location.search)
-  const history = useHistory()
-  const dispatch = useAppDispatch()
-  const {
-    isLoading,
-    queryParams,
-    allResult,
-    imageResult,
-    newsResult
-  } = useAppSelector((state) => state.google)
-  const [keyword, setKeyword] = useState(params.q)
-  const [currentTab, setCurrentTab] = useState(params.type)
+  const params = queryString(window.location.search);
+  const history = useHistory();
+  const dispatch = useAppDispatch();
+  const { isLoading, queryParams, allResult, imageResult, newsResult } =
+    useAppSelector((state) => state.google);
+  const [keyword, setKeyword] = useState(params.q);
+  const [currentTab, setCurrentTab] = useState(params.type);
 
   const onInputSearch = (val: string) => {
-    setKeyword(val)
-  }
+    setKeyword(val);
+  };
 
   const onSearch = () => {
     if (!isOnlyWhiteSpace(keyword)) {
-      const params = { ...queryParams, q: keyword }
-      history.push(`/result?${serializeQuery(params)}`)
+      const params = { ...queryParams, q: keyword };
+      history.push(`/result?${serializeQuery(params)}`);
     }
-  }
+  };
 
   const onSelectTab = (id: string) => {
-    const params = { ...queryParams, type: id }
-    history.push(`/result?${serializeQuery(params)}`)
-    setCurrentTab(id)
-  }
+    const params = { ...queryParams, type: id };
+    history.push(`/result?${serializeQuery(params)}`);
+    setCurrentTab(id);
+  };
 
   useEffect(() => {
     if (params.q) {
-      const newParams = { ...queryParams, ...params }
-      dispatch(setQueryParams(newParams))
+      const newParams = { ...queryParams, ...params };
+      dispatch(setQueryParams(newParams));
       if (params.type === EnumTabsResult.ALL) {
-        dispatch(searchContent(newParams))
+        dispatch(searchContent(newParams));
       } else if (params.type === EnumTabsResult.IMAGE) {
-        dispatch(searchImage(newParams))
+        dispatch(searchImage(newParams));
       } else if (params.type === EnumTabsResult.NEWS) {
-        dispatch(searchNews(newParams))
+        dispatch(searchNews(newParams));
       }
     }
-  }, [params.q, params.type])
+    if (
+      !params.q ||
+      ![EnumTabsResult.ALL, EnumTabsResult.IMAGE, EnumTabsResult.NEWS].includes(
+        params.type as EnumTabsResult
+      )
+    ) {
+      history.push("/");
+    }
+  }, [params.q, params.type]);
 
   return (
     <div className="flex flex-col gap-5 py-10">
@@ -91,11 +97,11 @@ const ResultPage = () => {
             {(() => {
               switch (currentTab) {
                 case EnumTabsResult.ALL:
-                  return <SearchAll data={allResult} />
+                  return <SearchAll data={allResult} />;
                 case EnumTabsResult.IMAGE:
-                  return <ImagesResult data={imageResult} />
+                  return <ImagesResult data={imageResult} />;
                 case EnumTabsResult.NEWS:
-                  return <NewsResult data={newsResult} />
+                  return <NewsResult data={newsResult} />;
                 default:
                   break;
               }
@@ -104,7 +110,7 @@ const ResultPage = () => {
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ResultPage
+export default ResultPage;
